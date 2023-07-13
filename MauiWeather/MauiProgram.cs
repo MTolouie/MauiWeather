717d4b1401979a331466df02ec55cbfb046da7ef
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2023-07-13 19:50:38 +0300

    Last Design Changes

--- MauiWeather/MauiProgram.cs
+++ MauiWeather/MauiProgram.cs
@@ -15,9 +15,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("Rubik-Light.ttf", "RubikLight");
-				fonts.AddFont("Rubik-Regular.ttf", "Rubik");
-			});
+                fonts.AddFont("Rubik-Light.ttf", "RubikLight");
+                fonts.AddFont("Rubik-Regular.ttf", "Rubik");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
